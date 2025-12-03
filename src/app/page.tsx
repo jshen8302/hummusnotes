df58b1b527bb27b32fb669c90d8569ee160155d6
James Shen <hummus@mac.mynetworksettings.com>
2025-12-02 23:44:55 -0500

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function HomePage() {
     <div className="flex flex-col">
 
       {/* Hero Section */}
-      <section className="h-[calc(100vh-4rem)] flex flex-col bg-amber-50 max-w-[740px]">
-        <div>
+      <section className="h-[calc(100vh-4rem)] flex flex-col max-w-[740px]">
+        <div className="mt-14 sm:mt-30 mb-7 sm:mb-15 hover:cursor-pointer">
           <Image 
             src="/mr-beast-thumbnail-money-pool.jpg"
             alt="Hero Banner"
@@ -17,10 +17,10 @@ export default function HomePage() {
             height={397}
           />
         </div>
-        <p className="text-4xl font-extrabold bg-amber-600">
+        <p className="text-2xl sm:text-5xl font-extrabold mb-2 sm:mb-5 hover:cursor-pointer">
           How Mr. Beast represents all that is left of The American Dream
         </p>
-        <div className="flex gap-2 bg-blue-500">
+        <div className="nav-links text-sm sm:text-xl text-[#919191] flex gap-2 mb-2 sm:mb-5">
           <a>
             James Shen
           </a>
@@ -28,17 +28,18 @@ export default function HomePage() {
             Nov 26, 2025
           </a>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae auctor arcu.
-          Mauris eget velit id erat vulputate fringilla quis id justo. 
-          Sed pharetra... <span>Read More</span>
-        </p>
+        <div className="text-lg sm:text-3xl">
+          <p className="line-clamp-3 sm:line-clamp-5">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc... 
+          </p>
+          <span className="text-[#919191]">Read More</span>
+        </div>
 
       </section>
 
       {/* Recent Blog Section */}
       <section className="h-screen bg-gray-50 p-8">
-       
+    
       </section>
       
     </div>

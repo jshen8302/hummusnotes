f849f211fab55fcb9d457fcf56159113446955fb
James Shen <hummus@mac.mynetworksettings.com>
2025-12-02 21:58:43 -0500

    testing header mobile responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,22 @@
 export default function Header() {
   return (
-    <header className="w-full flex py-4 border-b border-[#E5E5E5]">
-      <div className="font-bold text-4xl px-4 hover:cursor-pointer">
-        hummusnotes
+    <header className="w-full border-b border-[#E5E5E5]">
+      <div className="flex py-4 mx-auto max-width-[960px]">
+        <div className="font-bold text-4xl px-4 hover:cursor-pointer">
+          hummusnotes
+        </div>
+        <nav className="nav-links px-4 flex items-center text-xl font-bold gap-5">
+          <a>
+            About
+          </a>
+          <a>
+            Contact
+          </a>
+          <a>
+            Notes
+          </a>
+        </nav>
       </div>
-      <nav className="nav-links px-4 flex items-center text-xl font-bold gap-5">
-        <a>
-          About
-        </a>
-        <a>
-          Contact
-        </a>
-        <a>
-          Notes
-        </a>
-
-      </nav>
     </header>
   )
 
